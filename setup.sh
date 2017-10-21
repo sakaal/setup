25ffb5016a80fde078a54804f3734bd23fb9c96b
Sakari Maaranen <sam@iki.fi>
2017-10-22 02:41:34 +0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Simple server setup script for testing purposes only.
+# INSECURE - DO NOT USE ANYTHING LIKE THIS IN PRODUCTION.
+#
+# Copy this script (a single file) to /root/setup.sh and run it
+# after installing a Fedora server with root account only.
+# It installs Ansible, Git, and administrator local user accounts.
+#
+dnf install -y ansible git
+cd /root
+git init .
+git remote add -t \* -f origin https://github.com/sakaal/setup.git
+git checkout master
+ansible-playbook -v setup.yml
+rm -f setup.retry
