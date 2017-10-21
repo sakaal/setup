f0e50640eb66584b745cf3e19527f8310b67c158
Sakari Maaranen <sam@iki.fi>
2017-10-22 02:53:30 +0300

    Read only for group and others

--- setup.sh
+++ setup.sh
@@ -13,5 +13,6 @@ git init .
 git remote add -t \* -f origin https://github.com/sakaal/setup.git
 git fetch --all
 git reset --hard origin/master
+chmod og-wx setup.sh
 ansible-playbook -v setup.yml
 rm -f setup.retry
